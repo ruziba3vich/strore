f9cd6c0f6f3ba788b041f244bd31c1720c838efd
prodonik <rruzibayevich@gmail.com>
2024-04-04 08:31:32 +0500

    store

--- MASALALAR/fix_crash/main.go
+++ MASALALAR/fix_crash/main.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+type computer struct {
+	brand *string
+}
+
+func main() {
+	// Ushbu code da hatolik bor, shu ni izlab tog'rilash kerak
+	// Kutiliyotgan natija: brand: apple
+
+	var c computer
+	change(&c, "apple")
+	fmt.Printf("brand: %s\n", *(c.brand))
+}
+
+func change(c *computer, brand string) {
+	c.brand = &brand
+}
--- MASALALAR/reverse_string_ptr/main.go
+++ MASALALAR/reverse_string_ptr/main.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+func main() {
+	// Pointer yordamida string ni joyida (funksiya return qimidi) qaytaruvchi funktsiyani ishlab chiqing.
+	// Ex: hello => olleh
+	myStr := "hello"
+	ReverseString(&myStr)
+	fmt.Println(myStr)
+}
+
+func ReverseString(s *string) {
+	var str string
+	
+	for i := len(*s) - 1; i > -1; i -- {
+		str += string((*s)[i])
+	}
+	*s = str
+}
